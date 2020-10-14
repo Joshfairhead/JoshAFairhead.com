4219d945cf96d1effb35f3df2c044e8b3cb0d94f
Josh <JoshAFairhead@gmail.com>
2020-10-15 01:04:07 +0200

    Text and pics up to fleas

---
+++ src/pages/journey.vue
@@ -0,0 +1,102 @@
+<template>
+  <LayoutHero>
+    <div class="flex bg-white shadow-lg rounded-lg">
+      <div class="flex-1 p-10">
+        <h1 id="About">A generative journey </h1> <!--Add padding-->
+            <p>When you look around society at large you'll find that most people are specialists who do one thing well. In fact, society incentivises and reward such behaviour, which in turn shapes the behaviour of people living within our culture with a number of pathologies. <strong> I'm not one of those people. </strong> </p> 
+            <p> For whatever reason, I've never been satisfied doing "just one thing" and have a drive to transcend disciplines while including new skills. Such people are often called "polymaths", "generalists", "multipotentiates", "pantologists" or more simply "renaissance people". We're a minority in what seems like a sea of madness but historically the most curious cats out there seem to fall into these categories; Plato, Pythagoras, DaVinci, <a href="https://www.youtube.com/watch?v=hrcJz63En94&list=PLANorT3Y7McMKzxqTw8u8ykq3pe_6A0Tn"> Maynard James Keenen</a>, <a href="https://www.youtube.com/watch?v=KdnGPQaICjk">Ted Nelson</a>, <a href="https://www.dgmlive.com/in-depth/the-guitar-circle-book-table-of-contents">Robert Fripp (King Crimson)</a>, Steve Jobs and Sylvain Richard (20syl). </p>
+            
+            <p> <h3>This is my personal story, scroll to the bottom for a list of links! </h3> </p>
+            
+            <p>The first skill I remember getting reasonably good at was the guitar, however its also the speciality I'm probably least proficient in as I learnt without any particular framework or discipline! It was a practice that led to related endeavours like building a replica Gibson Les Paul and dabbling with home recording... Guitar building was a one time passion project but home recording eventually led to a <strong> Masters degree in Audio Technology</strong>.</p>
+            <p> <strong>Heres a picture of said guitar and a video of me performing some material I wrote: </strong></p>
+            <div class="flex justify-center grid-cols-2 gap-12 bg-primary-200 rounded-lg p-4 mb-4"> <!-- Justify centre is the closes to what I want but not evenly spaced -->
+              <div> <g-image src="~/assets/img/lesPaul.jpg" width="315" height="315" fit="contain">LesPaul</g-image> </div>
+              <div> <iframe width="560" height="315" src="https://www.youtube.com/embed/HYDHSt-9z0c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>
+            </div>
+            <p>As mentioned, playing the guitar opened up many doors; like many teenagers I was into rock music and I started out on an electric guitar but as my tastes matured I started to enjoy playing an acoustic. Not wishing to limit myself I started to pick up a cornicopia of other instruments; drums, bass, mandolin, harmonica, didgeredoo and just about anything else physically available to me. </p>
+            <p>En route to a masters in Audio I took a detour through the circus arts, dance and object manipulation... which along with board sports helped hone my physical dexterity and coordination. Juggling's in itself is super interesting and has its own symbol language called <a href="http://www.twjc.co.uk/jugglingnotations.html">siteswap</a> to describe types of throws! (as exhibited in some of the verrry old shots below - circa age 18)</p>
+            
+            <div class="flex justify-center grid-cols-2 gap-12 bg-primary-200 rounded-lg p-4 mb-4">
+              <div> <iframe width="560" height="315" src="https://www.youtube.com/embed/dpC7pGyaLLg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>
+            </div>
+            <p>This brings us up to the point of leaving school... The garden of forking paths could have gone many directions at that point but upon finishing I decided to take a year out, earn some cash to buy recording equipment to set myself up for more serious study over the next few years. </p>
+            <p>Somewhere over this stint, myself and a peer volunteered to do some live sound at a music festival and ended up running a small tent with no scheduled acts... and as such we ended up recruiting anyone we saw moving between stages with instruments! Wandering the waterlogged planes of the ironically names "Solace" festival we stumbled upon a local Cork band called Neon Flea Circus and invited them to play, they obliged with a blistering gig and we offered to record them. This project took on a life of its own across a variety of locations and with whatever equipment we could cobble together we recorded a creative commons album that <strong>absolutely bloody rocked!!</strong></p>
+            <p>Given we were releasing the art for free, I got very interested in the commerce aspect of music marketing and decided to do the album promotion for a final year project (along with recordings from another band we also met at Solace). Surprisingly we did manage to turn a buck from selling a free album in conjunction with a national tour and merchandise. </p>
+
+            <div class="flex rounded-lg"> 
+              <iframe class="rounded-lg mb-6" width="1150" height="678" src="http://neonfleacircus.com/download/index.php"></iframe>
+            </div>
+
+            <p>For my efforts in all this I received a first class honours or 1:1 in the English educational system which entitled me to a free masters at the same university which took two years (part-time). There were a few interesting projects over this time and as I delved into the subject of subjectivity I learned for myself the universal laws of vibration and how "the octave never repeats"... read this to mean that pythagorean tuning is fractal and we like tubes because their intermodulation distortion is musical! </p>
+            <p>Having worked on other peoples music for several years I decided to get self indulgent for my masters project by writing and recording my own concept album in 5.1 surround sound... I'm pretty happy with the whole package (lyrics, songwriting, musicianship and recording) but to be perfectly honest it wasn't as life affirming as the Fleas stuff I'd previously worked on. Still it scratched an itch and I got to wear many hats!</p>
+            <p>Post education I fell into live sound which broadened into running media centres and lights leading to work as a night club tech manager. This led me into forming a company providing these services and I ended up doing work with some pretty well known acts (one of the most notorious [dickheads in dance music](https://www.youtube.com/watch?v=8JAMkrzSRao) was a fan!). </p>
+            <p>During this time I built the sound system for several well regarded venues; namely Oriole, Swift and Ember. The former hosted live jazz every night and I was lucky enough to meet some of Londons finest musicians while destroying my liver on the booze! </p>
+            <p>Alas though, graveyard shifts at the weekend were slowly killing me and I felt an impetus to get out of entertainment. Digging through systemic legal corruption I followed the money and ended up learning about the global financial system, sickened by the money printing I decided to "buy out" of fiat currency into silver and gold. Pretty quickly this led to the topic of digital currency and I found bitcoin again, which I had a modicum of success in. This took me right into the heart of the crypto currency scene just as the DAO was failing... at this point I started buying ETH at $7 and soon enough it was ICO madness which I ingested every second of, learning the ins and outs of mechanism design and a nascent computing paradigm. </p>
+            <p>One of the ICOs I was watching at the time was a project called Pillar which was inviting people out to Slovakia where they were hosting an unconference so I went along and ended up hired to work on a wallet. This became one of the toughest learning experiences of my life! </p>
+            <p>Its still hard to articulate what the hell happened at that company over the next year or so but I've got at least a basic understanding of the patterns now. Summarised for brevity I'd say it was emergence left to do its thing while everyone held on for dear life. Spiral dynamics might be another way of putting it. </p>
+            <p>While there I did work on practically everything; ------------------ and learnt as much as possible. At no point did the place feel remotely "safe" and I knew that my participation needed to be artful. It was a masterclass in "dealism" and emergence as there was no formal authority (apart from amongst an illegitimate and self-elected peer group - MBA's). This taught me a lot about social situations and what not to do... </p>
+            <p>__I might also mention that while immersed in this chaos I thought it would be a good idea to take some plant medicine with a Shaman. Actually, I drank auyhuesca twice over this period.__</p>
+            <p>Eventually this time ended and I moved on to put the skills I'd developed into practice at Giveth, or more specifically in booting up the "Commons Stack" - which emerged out of seeding memes around Elinor Ostroms work "Governing the Commons". This was successful to large degrees and five teams got together to "meta-hack" on the project at Odyssey (while coliving on a pirate ship), but given my distaste for autocrats and a lack of income I was forced to depart from the group. </p>
+            <p>Around this time my mental models were starting to fall into place; I'd always read the kinds of books you'd find on an Ivy Leagues reading list like Kannaman, Taleb and other intellectual heavyweights - but at this point I was on to the likes of Gurdjieff and Robert Anton Wilson, the super weird and wonderful! Everything I'd learnt up until this point started to come together and integrate into a meta-model; I saw causal patterns everywhere and soon after I was recommended the Riso-Hudson enneagram model which unlocked the social matrix... </p>
+            <p>When I found Gurdjieffs work and realised I was already on a pretty good trajectory (he was clearly legit given my masters investigation was in the first chapter... the final chapter also clearly describes DNA about five years before its discovery). </p>
+            <p>---------------</p>
+            <p>The Commons stack having proved itself to be on the wrong path left me ready to start over - so I did! I put my stuff in a lock up and started traveling European hackathons/conferences and couch surfing with friends. After about half a year I'd built a small community of interesting personal connections in Telegram. </p>
+            <p>The start of 2020 brought with it Covid-19, just as I landed in Italy to stay with friends. Luckily, Liminal Village was in transformation for just such an occasion; plenty of room for everyone in the countryside, three acres to grow food on and lots of lock down time which I embraced. Just before arrival I was granted a scholarship for "Capra Course / The Systems View of Life", so I got deep into applied complex systems. </p>
+            <p>Over this period I also learnt a lot about Holochain (a Rust framework for distributed computing) while taking part on both archetecture and business courses they were running, as well as their community "hackalongs" which was an idea they integrated from us - and finally Devcamp where I got more confident with the code... </p>
+            <p>Bringing us up to today!</p>
+            <!--
+            - [[Portfolio]]
+                    - [Boris Brejcha @ Egg London 2017 Pt. 1](https://www.youtube.com/watch?v=k0wpI_IxWzA)
+                    - [Erick Morillo @Egg LDN 2016](https://www.youtube.com/watch?v=8JAMkrzSRao)
+                    - [Egg London going off](https://www.youtube.com/watch?v=R5l3Bc19_A4)
+                    - [Boris Brejcha @ Egg London (01.07.2017)](https://www.youtube.com/watch?v=Yems6-_PWHk)
+                    - [WILL ATKINSON LIVE @ KEARNAGE - TRANCE SANCTUARY - EGG LONDON (18-11-2017)](https://www.youtube.com/watch?v=B03-YqPajYg)
+                    - [Claptone - The Drums (Din Daa Daa) feat. George Kranz @ Egg London](https://www.youtube.com/watch?v=D-2O6_m8_L8)
+                    - [Noise & Girls at EGG LDN - Chris Thomas](https://www.youtube.com/watch?v=rg8KmUZJVvA)
+                    - [Dense & Pika @ Familia, EGG, London 14/01/2017 [Part 1/2]](https://www.youtube.com/watch?v=nUP709jlEq8)
+                    - [ISHU DEFT @ Egg London (Terrace)25/03/2017](https://www.youtube.com/watch?v=T9OCm2ZD6lk)
+                    - [Miky J - Egg London 28 - 01 - 2017 Part 1](https://www.youtube.com/watch?v=-nS0pmaWTc8)
+                -->
+      </div>
+    </div>
+  </LayoutHero>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Skills"
+  },
+
+  data() {
+    return {
+      cards: [
+        {
+          title: "this one has a link",
+          img: "https://picsum.photos/600/300",
+          link: "https://vuejs.org/v2/guide/components-props.html",
+          content:
+            "blanditiis animi sequi beatae nisi facilis maiores voluptate doloribus necessitatibus est laborum vel. Quasi, nisi. "
+        },
+        {
+          title: "hello world",
+          img: "card.png",
+          extraClassImg: "h-64",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis neque, mollitia a magni ducimus aliquam blanditiis animi sequi beatae nisi facilis maiores voluptate doloribus necessitatibus est laborum vel. Quasi, nisi. "
+        },
+
+        {
+          title: "No image here",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis neque, mollitia a magni ducimus aliquam blanditiis animi sequi beatae nisi facilis maiores voluptate doloribus necessitatibus est laborum vel. Quasi, nisi. "
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
