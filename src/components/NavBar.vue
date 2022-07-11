138f398eb97f09c19870e0b3da6be14994b20b15
Josh Fairhead <hello@joshafairhead.com>
2022-07-12 00:03:35 +0100

    Got rid of broken cal, changed blog address

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,12 @@
 <template>
-  <header class="header sticky top-0 bg-black-800 mb-5 opacity-75"> 
-    <!-- <div class="h-2 w-full bg-primary-500"></div> --> 
+  <header class="header sticky top-0 bg-black-800 mb-5 opacity-75">
+    <!-- <div class="h-2 w-full bg-primary-500"></div> -->
     <nav class="flex items-center justify-between flex-wrap py-6 container">
       <!-- site title / logo -->
       <div class="flex items-center flex-no-shrink mr-6">
         <strong>
-          <h1 class= text-primary-500> JoshAFairhead </h1> <!-- bump me right a little bit! -->
+          <h1 class="text-primary-500">JoshAFairhead</h1>
+          <!-- bump me right a little bit! -->
         </strong>
       </div>
       <!-- end site title / logo -->
@@ -13,7 +14,11 @@
       <!-- mobile navigation toggle -->
       <div class="block sm:hidden">
         <button @click="toggle" class="nav-toggle">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
@@ -23,12 +28,14 @@
 
       <!-- Navigation Links-->
       <div
-        :class="open ? 'block': 'hidden'"
+        :class="open ? 'block' : 'hidden'"
         class="w-full sm:flex sm:items-center sm:w-auto flex-grow sm:flex-grow-0"
       >
         <g-link class="nav-link" to="/">Home</g-link>
-        <g-link class="nav-link" to="https://calendar.google.com/calendar/embed?src=vaq0no7iet9g02imla1gm4btaisepnmo%40import.calendar.google.com&ctz=Europe%2FBerlin">Calendar</g-link>
-        <g-link class="nav-link" to="https://www.notablog.joshafairhead.com">NotABlog</g-link>
+        <!-- <g-link class="nav-link" to="https://calendar.google.com/calendar/embed?src=vaq0no7iet9g02imla1gm4btaisepnmo%40import.calendar.google.com&ctz=Europe%2FBerlin">Calendar</g-link> -->
+        <g-link class="nav-link" to="https://www.tothecosmos.com"
+          >NOTABLOG</g-link
+        >
         <!-- <g-link class="nav-link" to="/qualities/">Qualities</g-link> -->
         <!-- <g-link class="nav-link" to="/quantities/">Quantities</g-link> -->
       </div>
@@ -49,14 +56,14 @@ query {
 export default {
   data() {
     return {
-      open: false
+      open: false,
     };
   },
   methods: {
     toggle() {
       this.open = !this.open;
-    }
-  }
+    },
+  },
 };
 </script>
 
